9d179941497f476661400ad4a09e1a66ecceeafe
Andrew Bwogi <andrew.bwogi@gmail.com>
2023-03-15 21:23:58 -0700

    minor change

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("welcome from home");
+app.get('/', (req, res) => {
+	res.send('welcome from home');
 });
-app.get("/books", (req, res) => {
-  res.send("hahahahahahahhahahaaiidhiagduogauodhguagdigaiduygiuagduagdiu");
+app.get('/books', (req, res) => {
+	res.send('books route accessed');
 });
 app.listen(8000, () => {
-  console.log("server is lestining...");
+	console.log('server is lestining...');
 });
